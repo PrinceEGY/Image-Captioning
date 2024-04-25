abfe0b25fb549466025837bf94acf1908d40e113
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-04-25 22:23:43 +0200

    refactored to work on config files

--- inference.py
+++ inference.py
@@ -2,6 +2,8 @@ import os
 import urllib
 import urllib.request
 
+import yaml
+
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 from utils.utils import load_image
@@ -16,85 +18,72 @@ import argparse
 from models.rnn_model import RNNImageCaptioner
 from keras.applications import EfficientNetV2B3
 
-parser = argparse.ArgumentParser(description="Image captioning inference")
-parser.add_argument(
-    "-i",
-    "--image_path",
-    type=str,
-    required=True,
-    help="Path to the image file for caption generation, it can be either local image path or cloud image URI",
-)
 
-parser.add_argument(
-    "-m",
-    "--model",
-    type=str,
-    choices=["rnn", "transformer"],
-    default="rnn",
-    help="Choose the model you want to make inferences with (RNN model or transformer model)",
-)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Image captioning inference")
+    parser.add_argument(
+        "-i",
+        "--image_path",
+        type=str,
+        required=True,
+        help="Path to the image file or cloud image URI",
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        default=r"configs/rnn_config.yaml",
+        help="Path to the configuration file",
+    )
+    parser.add_argument(
+        "-w",
+        "--weights_path",
+        type=str,
+        default=r"weights/gru-feat-ep_150-loss_1.95.weights.h5",
+        help="Path to the model weights",
+    )
+    parser.add_argument(
+        "-g",
+        "--gen_method",
+        choices=["greedy", "beam"],
+        default="greedy",
+        help="Search strategy for caption generation",
+    )
+    parser.add_argument(
+        "-t",
+        "--temperature",
+        type=float,
+        default=0.0,
+        help="Temperature for generating captions (greedy search only)",
+    )
+    parser.add_argument(
+        "-k",
+        "--kbeams",
+        type=int,
+        default=3,
+        help="Number of beams for beam search",
+    )
+    return parser.parse_args()
 
-parser.add_argument(
-    "-w",
-    "--weights_ckp_path",
-    type=str,
-    default=r"weights\gru-feat-ep_50-loss_1.55.weights.h5",
-    help="Path to the weights of the Keras model (.weight.h5 file)",
-)
 
-parser.add_argument(
-    "-g",
-    "--gen_method",
-    choices=["greedy", "beam"],
-    default="greedy",
-    help="Search strategy (greedy or beam)",
-)
-
-parser.add_argument(
-    "-t",
-    "--temp",
-    type=float,
-    default=0.0,
-    help="Temperature for generating captions (greedy search only)",
-)
-
-parser.add_argument(
-    "-k",
-    "--kbeams",
-    type=int,
-    default=3,
-    help="Number of beams for beam search",
-)
-
-args = parser.parse_args()
-effnet = EfficientNetV2B3(include_top=False)
-effnet.trainable = False
-
-feature_extractor = FeatureExtractor(
-    r"data\flicker8k\Flicker8k_Dataset", feature_extractor=effnet
-)
-feature_extractor.load(r"weights\features.cache.pkl")
-
-preprocess = Preprocessor()
-tokenizer = Tokenizer.from_vocabulary(
-    path=r"weights\tokenizer_vocab.pkl",
-    standardize=preprocess,
-    ragged=True,
-)
+def load_config(config_path):
+    with open(config_path, "r") as file:
+        return yaml.safe_load(file)
 
 
 def load_model(weights_path):
-    if args.model == "rnn":
+    if config["model"]["type"] == "rnn":
         model = RNNImageCaptioner(
             tokenizer=tokenizer,
             feature_extractor=feature_extractor,
-            embedding_dim=256,
-            rnn_layers=1,
-            rnn_units=512,
+            **config["model"]["params"],
         )
-        model.build(input_shape=((None, 7, 7, 1536), (None, None)))
+        model.build(
+            input_shape=((None, *config["model"]["img_features_shape"]), (None, None))
+        )
         model.load_weights(weights_path)
-    elif args.model == "transformer":
+
+    elif config["model"]["type"] == "transformer":
         raise NotImplementedError("Transformer model not implemented")
     return model
 
@@ -104,16 +93,40 @@ def _is_url(path):
 
 
 if __name__ == "__main__":
+    args = parse_arguments()
+    config = load_config(args.config)
+
     if _is_url(args.image_path):
         image_path, _ = urllib.request.urlretrieve(args.image_path, "_temp.jpg")
     else:
         image_path = args.image_path
 
+    pooling = None
+    if config["model"]["params"]["pooling"] == False:
+        pooling = "avg"
+    effnet = EfficientNetV2B3(include_top=False, pooling=pooling)
+    effnet.trainable = False
+
+    feature_extractor = FeatureExtractor(
+        imgs_path=r"data\flicker8k\Flicker8k_Dataset",
+        feature_extractor=effnet,
+    )
+    if config["model"]["params"]["pooling"] == False:
+        feature_extractor.load(r"weights\features-pool.cache.pkl")
+    else:
+        feature_extractor.load(r"weights\features.cache.pkl")
+
+    tokenizer = Tokenizer.from_vocabulary(
+        path=r"weights\tokenizer_vocab.pkl",
+        standardize=Preprocessor(),
+        ragged=True,
+    )
+
     image = load_image(image_path)
-    model = load_model(args.weights_ckp_path)
+    model = load_model(args.weights_path)
 
     if args.gen_method == "greedy":
-        caption = model.greedy_gen(image, temperature=args.temp)
+        caption = model.greedy_gen(image, temperature=args.temperature)
     elif args.gen_method == "beam":
         caption = model.beam_search_gen(image, Kbeams=args.kbeams)
 
