4ad75fcf49bb0bbb6baae82b8a99ee6300a488b5
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-04-26 06:37:09 +0300

    updating default model to LSTM

--- inference.py
+++ inference.py
@@ -32,18 +32,18 @@ def parse_arguments():
         "-c",
         "--config",
         type=str,
-        default=r"configs/rnn_config.yaml",
+        default=r"configs/lstm_config.yaml",
         help="Path to the configuration file",
     )
     parser.add_argument(
         "-w",
         "--weights_path",
         type=str,
-        default=r"weights/gru-feat-ep_150-loss_1.95.weights.h5",
+        default=r"weights/lstm-emb256-rnn1.512-ep_150-loss_1.66.weights.h5",
         help="Path to the model weights",
     )
     parser.add_argument(
-        "-g",
+        "-m",
         "--gen_method",
         choices=["greedy", "beam"],
         default="greedy",
@@ -60,7 +60,7 @@ def parse_arguments():
         "-k",
         "--kbeams",
         type=int,
-        default=3,
+        default=4,
         help="Number of beams for beam search",
     )
     return parser.parse_args()
@@ -107,10 +107,7 @@ if __name__ == "__main__":
     effnet = EfficientNetV2B3(include_top=False, pooling=pooling)
     effnet.trainable = False
 
-    feature_extractor = FeatureExtractor(
-        imgs_path=r"data\flicker8k\Flicker8k_Dataset",
-        feature_extractor=effnet,
-    )
+    feature_extractor = FeatureExtractor(feature_extractor=effnet)
     if config["model"]["params"]["pooling"] == False:
         feature_extractor.load(r"weights\features-pool.cache.pkl")
     else:
