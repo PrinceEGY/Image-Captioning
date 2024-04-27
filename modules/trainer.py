b1352540aea80b72873c8949f0dca63a2333dd42
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-04-27 04:13:55 +0300

    renamed argument name

--- modules/trainer.py
+++ modules/trainer.py
@@ -2,8 +2,6 @@ import matplotlib.pyplot as plt
 import keras
 import os
 
-from numpy import save
-
 keras.utils.set_random_seed(2024)
 
 
@@ -42,7 +40,7 @@ class Trainer:
         self.model.compile(optimizer=self.optimizer, loss=self.loss_fn)
         if self.adapt_bias:
             self.model.output_layer.adapt(
-                self.train_ds.map(lambda inputs, labels: labels)
+                self.train_ds.map(lambda images, labels: labels)
             )
         history = self.model.fit(
             self.train_ds,
