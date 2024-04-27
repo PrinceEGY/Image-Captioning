d9762d98ee767a10c604d359bd7a6e0d1684d65a
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-04-28 01:26:10 +0300

    support byte images

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 
 
-def load_image(image_path, img_shape=(224, 224)):
-    img = tf.io.read_file(image_path)
-    img = tf.image.decode_image(img, channels=3, expand_animations=False)
-    img = tf.image.resize(img, img_shape)
-    return img
+def load_image(image, image_shape=(224, 224)):
+    if isinstance(image, str):
+        image = tf.io.read_file(image)
+    image = tf.image.decode_image(image, channels=3, expand_animations=False)
+    image = tf.image.resize(image, image_shape)
+    return image
