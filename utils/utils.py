3bd3e21299edcf4b7651dbf8dda0ac9ead440581
Ahmed Mohsen <ahmed25004@hotmail.com>
2024-04-29 01:27:54 +0300

    supporting read images for tf.Tensor

--- utils/utils.py
+++ utils/utils.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 
 def load_image(image, image_shape=(224, 224)):
-    if isinstance(image, str):
+    if isinstance(image, str) or isinstance(image, tf.Tensor):
         image = tf.io.read_file(image)
     image = tf.image.decode_image(image, channels=3, expand_animations=False)
     image = tf.image.resize(image, image_shape)
